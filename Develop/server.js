9a475b2b4e78774d649420464e7a5bdf3e1b97ed
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-10 23:55:58 -0700

    Got delete and readme

--- Develop/server.js
+++ Develop/server.js
@@ -12,17 +12,6 @@ app.use(express.static(path.join(__dirname, "/public/assets/css")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// function readJSON() {
-//     let jsonData = []
-
-//     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-//         if (err) throw err
-//         jsonData = JSON.parse(data)
-//         return jsonData
-//     })
-// };
-
-
 // Routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -44,36 +33,53 @@ app.post("/api/notes", (req, res) => {
     const newNotes = req.body;
     let jsonData = []
     fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        jsonData = JSON.parse(data);
+        jsonData.push(newNotes);
+            jsonData.forEach((item, i) => {
+                item.id = i + 1;
+            });
+        const dataString = JSON.stringify(jsonData);
+        fs.writeFile(
+            path.join(__dirname, "/db/db.json"), dataString, "UTF8", err => {
+                if (err) throw err;
+                return res.json(dataString);
+            }
+        );
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    let jsonData = []
+    fs.readFile("./db/db.json", (err, data) => {
       if (err) throw err;
       jsonData = JSON.parse(data);
-      jsonData.push(newNotes);
-      const dataString = JSON.stringify(jsonData);
+  
+      
+      const noteIndex = jsonData.findIndex(
+        (note) => note.id === parseInt(req.params.id)
+      );
+      
+      if (noteIndex === -1) {
+        return res.sendStatus(404);
+      }
+    
+      jsonData.splice(noteIndex, 1);
+      const dataString = JSON.stringify(jsonData);;
+  
       fs.writeFile(
-        path.join(__dirname, "/db/db.json"),dataString,"UTF8",err => {
+        path.join(__dirname, "/db/db.json"),
+        dataString,
+        "UTF8",
+        function (err) {
           if (err) throw err;
-          return res.json(dataString);
+          return res.sendStatus(200);
         }
       );
     });
   });
   
-  app.delete("/api/notes/:routeName", (req, res) => {
-    // finds the index of the matching character in the array
-    const notesIndex = notes.findIndex(
-      (note) => note.routeName === req.params.routeName
-    );
-    if (notesIndex === -1) {
-      // did not find matching character
-      // send error response code
-      return res.sendStatus(404);
-    }
-  
-    // removes the character if its found
-    notes.splice(notesIndex, 1);
-    // send ok response code
-    return res.sendStatus(200);
-  });  
-  
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
